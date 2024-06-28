60d71c55f0f05c453f6b76d6bb8ada8513d9275e
Joel Rojas <201101046@est.umss.edu>
2024-06-28 16:23:25 -0400

    TD-27: Agregar caso de prueba positivo del endpoint Get all labels

---
+++ tests/test_labels.py
@@ -0,0 +1,39 @@
+import pytest
+import jsonschema
+from src.login import get_token
+from src.utils.label import get_all_labels
+
+
+def test_get_all_labels():
+    schema = {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "string"
+                },
+                "name": {
+                    "type": "string"
+                },
+                "order": {
+                    "type": "integer"
+                },
+                "color": {
+                    "type": "string"
+                },
+                "is_favorite": {
+                    "type": "boolean"
+                }
+            },
+            "required": ["id", "name", "order", "color", "is_favorite"]
+        }
+    }
+    response = get_all_labels(get_token())
+    response_data = response.json()
+    try:
+        jsonschema.validate(instance=response_data, schema=schema)
+    except jsonschema.exceptions.ValidationError as err:
+        pytest.fail(f"JSON schema dont match {err}")
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
