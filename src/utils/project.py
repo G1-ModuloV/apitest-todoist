bdd4fc746bc2fc65c1bd5b092ba91b327160fc5f
Harold Mejia <haroldgabrielmejiamena@gmail.com>
2024-06-29 16:35:43 -0400

    TD-9: Cambio de nombre en metodos

--- src/utils/project.py
+++ src/utils/project.py
@@ -1,7 +1,7 @@
 import requests
 from config import BASE_URI
 
-def get_all_projects(token):
+def get_a_project(token):
     url = f'{BASE_URI}/rest/v2/projects/2335308589'
     headers = {
         'Authorization': f'Bearer {token}',
--- tests/projects/test_get_a_project.py
+++ tests/projects/test_get_a_project.py
@@ -1,14 +1,14 @@
 import pytest
 import jsonschema
 from src.login import get_token
-from src.utils.project import get_all_projects
+from src.utils.project import get_a_project
 from src.assertions.projects.get_a_project_assertions import assert_get_a_project_code_200, assert_get_a_project_json
 
 #Verificar respuesta exitosa con un ID de proyecto válido
 def test_get_a_project_tc01():
-    response = get_all_projects(get_token())
+    response = get_a_project(get_token())
     assert_get_a_project_code_200(response)
 #Verificar que el objeto JSON en la respuesta contenga los campos esperados
 def test_get_a_project_tc02():
-    response = get_all_projects(get_token())
+    response = get_a_project(get_token())
     assert_get_a_project_json(response)
