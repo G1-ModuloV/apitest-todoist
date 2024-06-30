9166cb73b2b997a0cd5bb4620a27d0a853611c92
Jessie V. Villamor Vargas <201604459@est.umss.edu>
2024-06-30 00:12:44 -0400

    TD-1: Correccion de observaciones

--- tests/projects/test_get_all_projects.py
+++ tests/projects/test_get_all_projects.py
@@ -3,26 +3,22 @@ from src.login import get_token
 import requests
 import jsonschema
 from src.utils.project import get_all_projects
+from src.assertions.projects.get_all_projects_assertions import assert_code_200, assert_array, assert_cotent_type_es_json
 
 # 1)  @Automation    @Functional    @Smoke       @Validar la obtención de todos los proyectos con un token de autenticación válido
 def test_get_all_projects_valid_token():
     response = get_all_projects(get_token())
-    # Verificar que la respuesta tiene un estado 200 OK
-    assert response.status_code == 200, "Expected status code 200 OK"
-    # Verificar que la respuesta contiene un array JSON con los proyectos del usuario
-    assert isinstance(response.json(), list), "Expected response to be a JSON array"
+    assert_array(response)
+    assert_code_200(response)
 
 
 # 7)  @Automation    @Functional    @Smoke       @Validar que el Content-Type de la respuesta es application/json
 def test_content_type_is_json():
     response = get_all_projects(get_token())
-    # Verificar que el Content-Type de la respuesta es application/json
-    assert response.headers["Content-Type"] == "application/json", "Expected Content-Type application/json"
+    assert_cotent_type_es_json(response)
 
 # 8)  @Automation    @Functional    @Smoke       @Validar que la respuesta tiene el estado 200 OK para una solicitud exitosa
 def test_response_status_is_200():
     response = get_all_projects(get_token())
-
-    # Verificar que la respuesta tiene un estado 200 OK
-    assert response.status_code == 200, "Expected status code 200 OK"
+    assert_code_200(response)
 
