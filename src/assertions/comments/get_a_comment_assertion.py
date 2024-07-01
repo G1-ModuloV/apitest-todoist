d27c32deea754f499bb70265889c2a83e02be98f
PabloFloresUmss <201300401@est.umss.edu>
2024-06-30 21:37:33 -0400

    comments added for test cases

--- src/assertions/comments/get_a_comment_assertion.py
+++ src/assertions/comments/get_a_comment_assertion.py
@@ -1,15 +1,19 @@
-
-from src.login import get_token
-from src.utils.comment import get_comment
-from src.assertions.comments.get_a_comment_assertion import assert_get_get_a_comment_success
+from src.utils.json_reader import read_a_json
+from src.assertions.schema_assertion import assert_schema
 
 #1. Verificar que el request retorna el comentario seleccionado usando un codigo de autenticación valido
-def test_a_comment():
-    response = get_comment(get_token())
-    assert_get_get_a_comment_success(response)
+def assert_get_get_a_comment_success(response):
+    schema = read_a_json("comment_schema.json")
+    assert_schema(response, schema)
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
 
 #2. Verificar que el request retorna un codigo de error usando un codigo de autenticación invalido
+
 #3. Verificar que el retorna un codigo de error usando un codigo de autenticacion nulo/vacio
+
 #4. Verificar que el request retorna un comentario especifico usando el id valido del comentario
+
 #5. Verificar que el request retorna un codigo de error usando un id de comentario invalido
+
 #6. Verificar que el request retorna un codigo de error usando el nulo/vacio del comentario
