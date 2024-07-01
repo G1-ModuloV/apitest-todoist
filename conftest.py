e95dafa7b325730cd3041d01dadf8570bd3f6eaa
Jessicka <44000225+JessickaMoya@users.noreply.github.com>
2024-07-01 00:19:01 -0400

    TD-12: Agregar todos los casos de uso para request Get All Comments

--- conftest.py
+++ conftest.py
@@ -40,3 +40,24 @@ def invalid_token():
 @pytest.fixture
 def no_token():
     return None
+
+# Get All Comments
+@pytest.fixture
+def all_comments_valid_project_id():
+    return "2335308589"
+
+@pytest.fixture
+def all_comments_valid_task_id():
+    return "2335320398"
+
+@pytest.fixture
+def all_comments_invalid_id():
+    return "98u234jnfk"
+
+@pytest.fixture
+def all_comments_deleted_project_id():
+    return "2335509282"
+
+@pytest.fixture
+def all_comments_deleted_task_id():
+    return "8164848731"
--- src/utils/comment.py
+++ src/utils/comment.py
@@ -1,8 +1,9 @@
 import requests
+from config import BASE_URI
 
 
-def get_all_comments(token):
-    url = "https://api.todoist.com/rest/v2/comments?task_id=8157452578"
+def get_all_comments_from_task(token, task_id):
+    url = f'{BASE_URI}/rest/v2/comments?task_id={task_id}'
     headers = {
         'Authorization': f'Bearer {token}',
     }
@@ -11,6 +12,13 @@ def get_all_comments(token):
 
 def get_comment(token):
     url = "https://api.todoist.com/rest/v2/comments/3567198885"
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    return requests.get(url, headers=headers)
+
+def get_all_comments_from_project(token, project_id):
+    url = f'{BASE_URI}/rest/v2/comments?project_id={project_id}'
     headers = {
         'Authorization': f'Bearer {token}',
     }
