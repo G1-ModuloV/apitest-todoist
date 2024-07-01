a99274fcee58da7c40f431d1d78ad5eec2b53d9c
GinoTorrico <sinnamewow2@gmail.com>
2024-07-01 17:21:09 -0400

    TD-23 agragar test cases y corregir error con development

---
+++ src/assertions/labels/get_a_personal_label_assertions.py
@@ -0,0 +1,20 @@
+from src.utils.json_reader import read_a_json
+from src.assertions.schema_assertion import assert_schema
+
+
+def assert_get_personal_label_valid_case(response):
+    assert response.status_code == 200
+
+
+def assert_get_personal_label_case_invalid_token(response):
+    assert response.status_code == 401
+
+def assert_get_personal_label_case_correct_date(response, label_id):
+    schema = read_a_json("personal_label_schema.json")
+    assert_schema(response, schema)
+    assert response.headers['Content-Type'] == 'application/json'
+    response_json = response.json()
+    assert response_json['id'] == label_id
+
+def assert_get_personal_label_case_not_found(response):
+    assert response.status_code == 404
