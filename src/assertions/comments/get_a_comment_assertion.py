e8206ded822c5706631bdb91ca1393e43b1e7424
PabloFloresUmss <201300401@est.umss.edu>
2024-07-01 00:47:58 -0400

    TD-16: correciones en base a observaciones en pull request

--- src/assertions/comments/get_a_comment_assertion.py
+++ src/assertions/comments/get_a_comment_assertion.py
@@ -2,11 +2,9 @@ from src.utils.comment import get_a_comment
 from src.utils.json_reader import read_a_json
 from src.assertions.schema_assertion import assert_schema
 
-#Verificar que el retorna un codigo de error usando un codigo de autenticacion nulo/vacio
 def assert_get_a_comment_code_401(response):
     assert response.status_code == 401
 
-#Verificar que el request retorna un comentario especifico usando el id valido del comentario
 def assert_get_a_comment_success(response, valid_comment_id):
     schema = read_a_json("comment_schema.json")
     assert_schema(response, schema)
@@ -15,6 +13,5 @@ def assert_get_a_comment_success(response, valid_comment_id):
     response_json = response.json()
     assert response_json['id'] == valid_comment_id
 
-#Verificar que el request retorna un codigo de error usando el nulo/vacio del comentario
 def assert_get_a_comment_code_400(response):
     assert response.status_code == 400
