b1bf27106a9045b5e78df98dd15e42bdfdba52a3
Jessie V. Villamor Vargas <201604459@est.umss.edu>
2024-07-06 17:29:41 -0400

    TD-8: Agregar casos de prueba para el endpoint Update a project

--- src/utils/project.py
+++ src/utils/project.py
@@ -29,3 +29,12 @@ def post_a_project_with_id(token, project_id, endpoint="https://api.todoist.com/
     url = f"{endpoint}/{project_id}"
     response = requests.post(url, headers=headers)
     return response
+
+
+def update_a_project(project_id_update, project_data, token):
+    url = f"{BASE_URI}/rest/v2/projects/{project_id_update}"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+    }
+    return requests.post(url, headers=headers, data=project_data)
