6efdb9977709b5dc61879b04b4f2886d784a4cd7
Yara Romero <yaraleromeral@gmail.com>
2024-07-06 19:49:40 -0400

    TD-15: Agregando 3 test cases de tipo regression

--- tests/tasks/test_post_create_a_task.py
+++ tests/tasks/test_post_create_a_task.py
@@ -3,6 +3,8 @@ from src.utils.task import create_task
 from src.assertions.tasks.post_create_task_assertions import (
     assert_create_task_successful,
     assert_create_a_task_json,
+    assert_create_task_bad_request,
+    assert_create_task_unauthorized
 )
 
 @pytest.mark.smoke
@@ -29,4 +31,22 @@ def test_create_task_response_format(valid_task_data_mandatory_field, valid_toke
     assert_create_task_successful(response)
     assert_create_a_task_json(response)
 
+@pytest.mark.regression
+# TD-15 Verificar que no se pueda crear una tarea sin el parámetro obligatorio "content".
+def test_create_task_without_mandatory_param(incomplete_task_data, valid_token):
+    response = create_task(incomplete_task_data, valid_token)
+    assert_create_task_bad_request(response)
 
+
+@pytest.mark.regression
+# TD-15 Verificar que no se pueda crear una tarea con un token de autorización inválido
+def test_create_task_with_invalid_token(valid_task_data_optional_fields, invalid_token):
+    response = create_task(valid_task_data_optional_fields, invalid_token)
+    assert_create_task_unauthorized(response)
+
+
+@pytest.mark.regression
+# TD-15 Verificar que no se pueda crear una tarea sin token de autorización
+def test_create_task_without_token(valid_task_data_mandatory_field, no_token):
+    response = create_task(valid_task_data_mandatory_field, no_token)
+    assert_create_task_unauthorized(response)
