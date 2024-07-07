269ec92aaf4475d00324ad93e83060d22f71a93f
GinoTorrico <sinnamewow2@gmail.com>
2024-07-06 23:27:38 -0400

    TD-24: create a new personal label añadir casos de prueba de smoke test y mas correciones dadas

--- tests/labels/test_personal_label.py
+++ tests/labels/test_personal_label.py
@@ -7,6 +7,7 @@ from src.assertions.labels.get_a_personal_label_assertions import (assert_get_pe
 
 
 @pytest.mark.smoke
+@pytest.mark.regression
 # TD-23 Verificar respuesta exitosa con un ID valido y un token de autenticación válido
 def test_get_personal_label_valid_case(valid_label_id, valid_token):
     response = get_a_personal_label(valid_label_id, valid_token)
@@ -20,6 +21,7 @@ def test_get_personal_label_case_invalid_token(valid_label_id, invalid_token):
 
 
 @pytest.mark.smoke
+@pytest.mark.regression
 # TD-23 Verificar respuesta correcta de campos esperados
 def test_get_personal_label_case_correct_date(valid_label_id, valid_token):
     response = get_a_personal_label(valid_label_id, valid_token)
@@ -32,6 +34,7 @@ def test_assert_get_personal_label_case_not_found(invalid_label_id, valid_token)
     assert_get_personal_label_case_not_found(response)
 
 @pytest.mark.smoke
+@pytest.mark.regression
 # TD-23 Verificar respuesta correcta de campos esperados en query param
 def test_get_personal_label_case_query_param_correct_date(valid_query_param_label_id, valid_token):
     response = get_a_personal_label(valid_query_param_label_id, valid_token)
