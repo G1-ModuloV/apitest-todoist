31a9d23d11150a473212b1671cfdf0b3d53c7333
Yara Romero <yaraleromeral@gmail.com>
2024-07-06 20:53:47 -0400

    TD-15: Modificando los comentarios observados

--- tests/tasks/conftest.py
+++ tests/tasks/conftest.py
@@ -4,15 +4,15 @@ from src.utils.task import create_task, delete_task
 @pytest.fixture(scope="session")
 def valid_task_data_mandatory_field():
     return {
-        "content": "Nueva tarea 1"
+        "content": "Test nueva tarea"
     }
 
 
 @pytest.fixture
 def valid_task_data_optional_fields():
     return {
-        "content": "New Task holaaa",
-        "description": "Description of the new task",
+        "content": "Tarea con campos opcionales",
+        "description": "Esta tarea es con campos opcionales",
         "due_date": "2024-07-10"
     }
 
@@ -20,14 +20,13 @@ def valid_task_data_optional_fields():
 @pytest.fixture
 def incomplete_task_data():
     return {
-        "description": "Description of the new task",
+        "description": "Nueva descripción",
         "due_date": "2024-07-10"
     }
 
 
 @pytest.fixture(scope="session")
 def setup_create_task(valid_task_data_mandatory_field, valid_token):
-    # Setup: Crea una tarea de prueba
     response = create_task(valid_task_data_mandatory_field, valid_token)
     task_id = response.json()['id']
 
