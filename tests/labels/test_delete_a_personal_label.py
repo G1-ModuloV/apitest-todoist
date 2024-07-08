7218118b8111c4498d545988e266dae38ae73cc8
Joel Rojas <201101046@est.umss.edu>
2024-07-08 11:01:21 -0400

    TD-45: Agregar casos de prueba de tipo regresion para el endpoint delete a label

--- tests/labels/test_delete_a_personal_label.py
+++ tests/labels/test_delete_a_personal_label.py
@@ -2,6 +2,8 @@ import pytest
 from src.utils.label import delete_a_label
 from src.assertions.labels.delete_a_label_assertions import assert_delete_a_label_successfully, \
     assert_delete_a_label_forbidden
+from config import BASE_URI
+import requests
 
 
 @pytest.mark.smoke
@@ -17,4 +19,25 @@ def test_delete_a_label_valid_case(get_valid_label_id, valid_token):
 # TD-45 Verificar la eliminación de una etiqueta con un id invalido y un token de autenticación invalido
 def test_delete_a_label_invalid_case(nonexistent_label_id, invalid_token):
     response = delete_a_label(nonexistent_label_id, invalid_token)
+    assert_delete_a_label_forbidden(response)
+
+
+@pytest.mark.regression
+# TD-45 Verificar la eliminación de una etiqueta con un id valido y un token de autenticación invalido
+def test_delete_a_label_bad_token(get_valid_label_id, invalid_token):
+    response = delete_a_label(get_valid_label_id, invalid_token)
+    assert_delete_a_label_forbidden(response)
+
+
+@pytest.mark.regression
+# TD-45 Verificar la eliminación de una etiqueta con un id invalido y un token de autenticación valido
+def test_delete_a_label_bad_id(nonexistent_label_id, valid_token):
+    response = delete_a_label(nonexistent_label_id, valid_token)
+    assert_delete_a_label_successfully(response)
+
+
+@pytest.mark.regression
+# TD-45 Verificar la eliminación de una etiqueta con un id valido y ningun tipo de autenticación
+def test_delete_a_label_without_authorization(get_valid_label_id):
+    response = requests.delete(f"{BASE_URI}/rest/v2/labels/{get_valid_label_id}", headers={})
     assert_delete_a_label_forbidden(response)
