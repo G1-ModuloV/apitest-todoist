22fd6534b7d8369daa651236a91e02ece2080ad7
PabloFloresUmss <201300401@est.umss.edu>
2024-07-08 22:02:42 -0400

    TD-10: resolver merge conflicts

--- tests/comments/test_delete_comment.py
+++ tests/comments/test_delete_comment.py
@@ -1,6 +1,5 @@
 import pytest
-import json
-from src.utils.comment import delete_a_comment
+from src.utils.comment import delete_comment
 from src.assertions.comments.delete_a_comment_assertion import assert_delete_a_comment_success, \
     assert_delete_a_comment_unauthorized
 
@@ -10,7 +9,7 @@ from src.assertions.comments.delete_a_comment_assertion import assert_delete_a_c
 #TD-10 Verificar que se elimine un comentario con id valido
 def test_delete_comment(setup_and_teardown_delete_comment, valid_token):
     comment_id = setup_and_teardown_delete_comment
-    response = delete_a_comment(valid_token, comment_id)
+    response = delete_comment(valid_token, comment_id)
     assert_delete_a_comment_success(response)
 
 @pytest.mark.smoke
@@ -18,5 +17,5 @@ def test_delete_comment(setup_and_teardown_delete_comment, valid_token):
 #TD-10 Verificar que el request retorna un codigo de error usando un codigo de autenticación invalido
 def test_delete_comment_invalid_token(setup_and_teardown_delete_comment, invalid_token):
     comment_id = setup_and_teardown_delete_comment
-    response = delete_a_comment(invalid_token, comment_id)
+    response = delete_comment(invalid_token, comment_id)
     assert_delete_a_comment_unauthorized(response)
