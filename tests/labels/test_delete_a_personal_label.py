6b699053de63f66d37923a5755b0df1e5d535094
Joel Rojas <201101046@est.umss.edu>
2024-07-09 21:59:20 -0400

    TD-50: Actualizar el nombre de las funciones setup

--- tests/labels/test_delete_a_personal_label.py
+++ tests/labels/test_delete_a_personal_label.py
@@ -3,15 +3,14 @@ from src.utils.label import delete_a_label
 from src.assertions.labels.delete_a_personal_label_assertions import assert_delete_a_label_successfully, \
     assert_delete_a_label_forbidden
 import requests
-
 from src.utils.url import label_url
 
 
 @pytest.mark.smoke
 @pytest.mark.regression
 # TD-45 Verificar la eliminación de una etiqueta con un id valido y un token de autenticación valido
-def test_delete_a_label_valid_case(get_valid_label_id, valid_token):
-    response = delete_a_label(get_valid_label_id, valid_token)
+def test_delete_a_label_valid_case(setup_create_a_label, valid_token):
+    response = delete_a_label(setup_create_a_label, valid_token)
     assert_delete_a_label_successfully(response)
 
 
@@ -25,8 +24,8 @@ def test_delete_a_label_invalid_case(nonexistent_label_id, invalid_token):
 
 @pytest.mark.regression
 # TD-45 Verificar la eliminación de una etiqueta con un id valido y un token de autenticación invalido
-def test_delete_a_label_bad_token(get_valid_label_id, invalid_token):
-    response = delete_a_label(get_valid_label_id, invalid_token)
+def test_delete_a_label_bad_token(setup_create_a_label, invalid_token):
+    response = delete_a_label(setup_create_a_label, invalid_token)
     assert_delete_a_label_forbidden(response)
 
 
@@ -39,6 +38,6 @@ def test_delete_a_label_bad_id(nonexistent_label_id, valid_token):
 
 @pytest.mark.regression
 # TD-45 Verificar la eliminación de una etiqueta con un id valido y ningun tipo de autenticación
-def test_delete_a_label_without_authorization(get_valid_label_id):
-    response = requests.delete(label_url(get_valid_label_id), headers={})
+def test_delete_a_label_without_authorization(setup_create_a_label):
+    response = requests.delete(label_url(setup_create_a_label), headers={})
     assert_delete_a_label_forbidden(response)
